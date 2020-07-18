38a185d1cf3c1a738a26469a86943b52bcb0e9e8
jroma96 <romaj1805@gmail.com>
2020-07-18 11:28:29 -0700

    update

--- assets/js/scriptLogin.js
+++ assets/js/scriptLogin.js
@@ -3,9 +3,11 @@ function login() {
     xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
         var user = document.getElementById("user_id").innerText;
-        var user = document.getElementById("user_id").innerText;
+        var user = document.getElementById("user_pass").innerText;
+
       }
     };
-    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.open("GET", "https://8m717iy4bh.execute-api.us-east-1.amazonaws.com/Prod/perfil?email="+user, true);
     xhttp.send();
+    
   }
